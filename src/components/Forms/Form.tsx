5f7d12601f73beacbc75168b840fe92777084723
leoramosa <leo.ramos.ari@gmail.com>
2024-10-22 03:35:10 -0300

    web vite new

--- src/components/Forms/Form.tsx
+++ src/components/Forms/Form.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./form.css";
 import FormSignup from "./FormSignup";
 import FormSuccess from "./FormSuccess";
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 const NotFound = () => {
   return (
     <div>
       <h1>NotFount</h1>
     </div>
-  )
-}
+  );
+};
 
 export default NotFound;
